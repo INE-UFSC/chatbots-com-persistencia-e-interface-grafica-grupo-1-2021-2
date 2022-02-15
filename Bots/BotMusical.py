1957bd90e73afe898b391ac02c912d2733938bbf
gabi-fs <gabrielafdasilveira@gmail.com>
2022-02-15 10:53:20 -0300

    ajustando bots

--- Bots/BotMusical.py
+++ Bots/BotMusical.py
@@ -3,13 +3,14 @@ from Bots.Bot import Bot
 
 class BotMusical(Bot):
     def __init__(self, nome):
-        self.__nome = nome
+        super().__init__(nome)
         self.__comandos = [self.cria_comando(
             1, 'Bom dia', ['Alguma coisa acontece no meu coração... Ah, oi! Bom dia!']),
             self.cria_comando(2, 'Quem é você?', [
                               f'EU SOU O SAMBAAA! Brincadeira, eu sou {self.__nome}!']),
             self.cria_comando(3, 'Como vai ser o futuro?',
-                              ['Eu vejo a vida melhor no futuro, eu vejo isso por cima de um muro\nde hipocrisia que insiste em nos rodear...'
+                              ['Eu vejo a vida melhor no futuro, eu vejo isso por cima de um muro\nde hipocrisia que insiste em nos rodear...',
+                               'Sei que nada será como está, amanhã ou depois de amanhã...'
                                ]),
             self.cria_comando(4, 'Adeus', [
                               'Deixe-me ir, preciso andar, vou por aí a procurar... Rir pra não chorar! Tchaau!',
@@ -20,17 +21,9 @@ class BotMusical(Bot):
     def comandos(self):
         return self.__comandos
 
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-
     def boas_vindas(self):
         return(self.__comandos[0].get_resposta_random())
-        
+
     def despedida(self):
         return(self.__comandos[-1].get_resposta_random())
 
