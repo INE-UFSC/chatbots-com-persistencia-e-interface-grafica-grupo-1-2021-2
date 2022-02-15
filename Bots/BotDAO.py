f55c1bfe2fd905e7578a0a31d9291f17721f507d
TorresLino <arthur.torres.de.lino@gmail.com>
2022-02-15 11:53:01 -0300

    coisas para adicionar

--- Bots/BotDAO.py
+++ Bots/BotDAO.py
@@ -3,3 +3,11 @@ from .DAO import DAO
 class BotDAO(DAO):
     def __init__(self):
         super().__init__('bots.pkl')
+
+    #adiciona bots com base no local do arquivo
+    def importBot(self, local_arquivo_bot):
+        pass
+
+    #escreve o bot num arquivo separado
+    def exportBot(self, nome_bot):
+        pass
